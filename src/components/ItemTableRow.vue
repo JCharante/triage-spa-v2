36320f50a345fbba7730d7e0eecf2c393c8233f6
jcharante <git@jcharante.com>
2019-09-15 19:52:05 -0400

    finish editing row and set branding color

--- src/components/ItemTableRow.vue
+++ src/components/ItemTableRow.vue
@@ -26,9 +26,45 @@
                 </q-input>
             </q-popup-edit>
         </q-td>
+        <q-td key="hardDeadline" :props="props">
+            {{ hardDeadline }}
+            <q-popup-edit buttons v-model="hardDeadline_" @save="hardDeadline = hardDeadline_">
+                <q-input v-model="hardDeadline_">
+                    <template v-slot:append>
+                        <q-icon name="event" class="cursor-pointer">
+                            <q-popup-proxy ref="qDateProxy2" transition-show="scale" transition-hide="scale">
+                                <q-date v-model="hardDeadline_" @input="() => $refs.qDateProxy2.hide()" />
+                            </q-popup-proxy>
+                        </q-icon>
+                    </template>
+                </q-input>
+            </q-popup-edit>
+        </q-td>
         <q-td key="priorityPoints" :props="props">
             {{ props.row.priorityPoints }}
         </q-td>
+        <q-td key="difficulty" :props="props">
+            <q-select dense class="col" v-model="difficulty" :options="['', 'Easy', 'Medium', 'Hard']"/>
+        </q-td>
+        <q-td key="importance" :props="props">
+            <q-select dense class="col" v-model="importance" :options="['', 'Low', 'Medium', 'High', 'Mission Critical']"/>
+        </q-td>
+        <q-td key="status" :props="props">
+            <q-select dense class="col" v-model="status" :options="[`Not Started`, 'In Progress', 'Done', 'Delete']"/>
+        </q-td>
+        <q-dialog v-model="showDeletePrompt" persistent>
+            <q-card>
+                <q-card-section class="row items-center">
+                    <q-avatar icon="delete" color="primary" text-color="white" />
+                    <span class="q-ml-sm">This will delete this item.</span>
+                </q-card-section>
+
+                <q-card-actions align="right">
+                    <q-btn flat label="Cancel" color="primary" v-close-popup />
+                    <q-btn flat label="Yes, Delete." color="primary" v-close-popup @click="deleteItemById({ id: props.key })"/>
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
     </q-tr>
 </template>
 
@@ -113,22 +149,28 @@
                     return this.getItemById(this.props.key).status;
                 },
                 set(newVal) {
-                    return this.setItemPropertiesById({
-                        id: this.props.key,
-                        status: newVal,
-                    });
+                    if (newVal !== 'Delete') {
+                        return this.setItemPropertiesById({
+                            id: this.props.key,
+                            status: newVal,
+                        });
+                    }
+                    this.showDeletePrompt = true;
+                    return new Promise((resolve, reject) => resolve());
                 },
             },
         },
         methods: {
             ...mapActions([
                 'setItemPropertiesById',
+                'deleteItemById',
             ]),
         },
         mounted() {
             this.task_ = this.task;
             this.tags_ = this.tags;
             this.recommendedDeadline_ = this.recommendedDeadline;
+            this.hardDeadline_ = this.hardDeadline;
         },
         data() {
             return {
@@ -136,9 +178,7 @@
                 tags_: '',
                 recommendedDeadline_: '',
                 hardDeadline_: '',
-                difficulty_: '',
-                importance_: '',
-                status_: '',
+                showDeletePrompt: false,
             };
         },
     };
