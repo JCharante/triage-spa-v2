c6cf2be913af48f5be2b797e697b0dcd1d5c696d
jcharante <git@jcharante.com>
2020-02-15 18:37:36 -0500

    change root url

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,21 +2,21 @@
     <q-page class="flex flex-center">
         <div class="row" style="width: 390px; max-width: 90%;">
             <div class="col-12">
-                <q-input v-model="username" label="Username"/>
+                Log in or sign up with your phone number
             </div>
-            <div class="col-12">
-                <q-input v-model="password" label="Password" type="password"/>
-            </div>
-            <div class="col-12" v-if="creatingAccount">
-                <q-input v-model="displayName" label="Display Name"/>
-            </div>
-            <hr>
-            <div class="col-12 row">
-                <div class="col">
-                    <q-btn @click="creatingAccount ? signup() : signin()" :label="creatingAccount ? `Create Account` : `Sign in`" color="primary" :disable="(username.length === 0 || password.length === 0) || creatingAccount ? displayName.length === 0 : false"/>
+            <template>
+                <div class="col-12">
+                    <VueTelInput v-model="tel" :preferredCountries="['US', 'CA', 'VN', 'AU', 'CN', 'TH']"/>
                 </div>
-
-                <q-btn :label="!creatingAccount? `Need an account?` : `I already have an account`" @click="creatingAccount = !creatingAccount" color="secondary"/>
+                <div class="col-12" style="margin-top: 30px;">
+                    <q-btn label="Send Verification Code"/>
+                </div>
+            </template>
+            <template style="margin-top: 50%">
+                <q-input/>
+            </template>
+            <div class="col-12 row" style="margin-top: 50px">
+                <p>Why a phone number?</p>
             </div>
         </div>
     </q-page>
@@ -25,12 +25,17 @@
 
 <script>
     import { Notify } from 'quasar';
+    import { VueTelInput } from 'vue-tel-input';
     import axios, { axiosInstance } from '../boot/axios';
 
     export default {
         name: 'Login',
+        components: {
+            VueTelInput,
+        },
         data() {
             return {
+                tel: '',
                 username: '',
                 password: '',
                 displayName: '',
